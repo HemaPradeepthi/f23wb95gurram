3e9e809fe5ff49c79498b5ef7a76922882ac67b3
Hema Pradeepthi <ghpradeepthi24@gmail.com>
2023-11-01 17:43:55 -0500

    added endpoint

--- routes/Electronics.js
+++ routes/Electronics.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Search Results Electronics' });
+  res.render('Electronics', { title: 'Search Results Electronics' });
 });
 
 module.exports = router;
